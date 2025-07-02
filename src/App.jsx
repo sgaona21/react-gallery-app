1560989b3ebe20b5cc85c0d8e5925ee3ffc8c176
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-07-02 16:22:12 -0700

    updated gitignore
    added apikey to config.js and added config.js to gitignore file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import apiKey from './config'
+
 function App() {
   const [count, setCount] = useState(0)
 
